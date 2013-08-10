b1bc236b036437cb611465ade74ebc77046a9e50
huanzhang22 <huanzhang22@yahoo.com>
2013-08-09 21:05:30 -0400

    Revert "Revert "more""
    This reverts commit ad492e17c293caecf98bfd2e698b52785ded0349.

--- DbImportGUI/src/DbImportGUI/gui/GUIConfig.java
+++ DbImportGUI/src/DbImportGUI/gui/GUIConfig.java
@@ -1,7 +1,17 @@
 package DbImportGUI.gui;
 
+import java.util.*;
 public class GUIConfig {
 
 	public String title;
+	public ArrayList<DbInfo> dbArray;
+	
+	public void defaultInit(){
+		this.title = "Import Data File to Database";
+		this.dbArray = new ArrayList<DbInfo>();
+	}
+	public void initFromFile(){
+		
+	}
 	
 }
